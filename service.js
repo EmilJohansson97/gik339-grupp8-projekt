2d3dfcee8bf9e269ac6bf24c21b29424ea900aa9
EmilJohansson97 <h24joemi@du.se>
2026-01-06 09:57:02 +0100

    Backend klart

--- service.js
+++ service.js
@@ -6,20 +6,22 @@ const app = express();
 app.use(express.json());
 const db = new sqlite3.Database('cars.db');
 
-app.listen(port), () => {
+app.listen(port, () => {
     console.log(`Server running on port ${port}`);
-};
+});
 
 db.serialize(() => {
-    db.run(`CREATE TABLE IF NOT EXISTS cars (
+    db.run(
+        `CREATE TABLE IF NOT EXISTS cars (
         ID INTEGER PRIMARY KEY AUTOINCREMENT,
         Brand TEXT NOT NULL,
-        Color TEXT NOT NULL
-    )`);
+        Color TEXT NOT NULL)`
+    );
 });
 
 app.get('/cars', (req, res) => {
-    db.all('SELECT * FROM cars', [], (err, rows) => {
+    db.all(
+        'SELECT * FROM cars', [], (err, rows) => {
         if (err) {
             console.error(err);
             res.status(500).send('Error fetching cars');
@@ -32,7 +34,8 @@ app.get('/cars', (req, res) => {
 app.post('/cars', (req, res) => {
     const {brand, color} = req.body;
 
-    db.run('INSERT INTO cars VALUES ?, ?', {brand, color}, (err) => {
+    db.run(
+        'INSERT INTO cars VALUES (?, ?)', [brandrand, color], (err) => {
         if (err) {
             console.error(err);
             res.status(500).send('Error inserting into cars');
@@ -42,6 +45,31 @@ app.post('/cars', (req, res) => {
     })
 })
 
+app.delete('/cars/:id', (req, res) => {
+    const id = req.params.id;
 
+    db.run(
+        'DELETE FROM cars WHERE ID = ?', [id], (err) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send('Error deleting from cars');
+        } else {
+            res.json({message: 'Car deleted successfully'});
+        }
+    })
+})
 
+app.put('/cars', (req, res) => {
+    const {id, brand, color} = req.body;
+
+    db.run(
+        'UPDATE cars SET Brand = ?, Color = ? WHERE ID = ?', [brand, color, id], (err) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send('Error updating cars')
+        } else {
+            res.json({message: 'Car updated successfully'});
+        }
+    })
+})
 // db.run('INSERT INTO cars (Brand, Color) VALUES (?, ?)', ['Volvo', 'Red']);
